d9aaf65784cb5e797fae347635c88c3b66b5255c
Daniel Park <danielpark@Daniel-Parks-Macbook-Pro.local>
2019-09-08 21:08:38 +0900

    :package: Add environment variables

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const crypto = require('crypto');
-const salt = require('./.saltkey.js')
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const crypto = require("crypto");
+const bodyParser = require("body-parser");
+require("dotenv");
 
 const app = express();
 
 app.use(cors());
 
 app.listen(process.env.PORT || 5001, () => {
-  console.log('Listening on 5001 ...')
+  console.log("Listening on 5001 ...");
 });
 
 const user = [];
 
 app.use(bodyParser.json());
 
-app.post('/login', (req, res) => {
-  let hashedPassword = crypto.createHash('sha256')
-    .update(req.body.password + salt)
-    .digest('hex');
+app.post("/login", (req, res) => {
+  let hashedPassword = crypto
+    .createHash("sha256")
+    .update(req.body.password + process.env.SALT_KEY)
+    .digest("hex");
   let data = {
     email: req.body.email,
     password: hashedPassword
-  }
+  };
   user.push(data);
-  res.statusCode = 200;
-  res.send('ok');
-})
+  res.status(200).send("ok");
+});
